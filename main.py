d4da15c792862e6bb97189496e34e95902ce867c
Salvatore D'Angelo <sasadangelo@gmail.com>
2023-09-16 18:45:57 +0200

    Improve general report

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     folder = 'gpx'
 
     table_data = []
-    headers = ["Date", "Name", "Distance (Km)", "Duration", "Pace (min/Km)", "Max HR", "Avg HR", "Max Cadence", "Avg Cadence", "Elev. Gain", "Elev. Loss"]
+    headers = ["Date", "Name", "Distance (Km)", "Duration", "Pace (min/Km)", "Avg HR", "Elev. Gain"]
 
     for filename in os.listdir(folder):
         if filename.endswith('.gpx'):
@@ -29,12 +29,8 @@ def main():
                     f'{activity.get_distance():.2f}',
                     seconds_to_hhmmss(activity.get_duration()),
                     seconds_to_mmss(activity.get_average_pace()),
-                    activity.get_max_heart_rate(),
                     activity.get_average_heart_rate(),
-                    activity.get_max_cadence(),
-                    activity.get_average_cadence(),
-                    f'{activity.get_elevation_gain():.1f}',
-                    f'{activity.get_elevation_loss():.1f}'
+                    f'{activity.get_elevation_gain():.1f}'
                 ])
             except Exception as e:
                 print(f"Error: {str(e)}")
